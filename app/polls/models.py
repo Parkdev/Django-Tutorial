2bce336de68534c418dc5d5abb8f7763bc676233
Jeeho Park <devpark29@gmail.com>
2018-10-02 15:47:02 +0900

    Question에 was_published_recently()함수 구현, Question, Choice의 __str__() 설정

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,9 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('게시일자') #게시일자는 사람이 읽는 설명; 안써도되지만 쓰는걸 권장
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        최근에 게시되었는지 여부를 리턴
+
+        자신의 게시일자 >= 지금 - 하루
+        :return:  24시간이 지나지않았으면 참, 지났으면 거짓
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) #참 or 거짓을 리턴
 
 
 class Choice(models.Model):
@@ -13,3 +28,6 @@ class Choice(models.Model):
     )
     choice_text = models.CharField(max_length=200)
     vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- app/polls/urls.py
+++ app/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # /polls/
     path('test/', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='datail'),
+    path('<int:question_id>/results/',views.result, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
