fb5111af7808e38ffd8d2af486fd2e2efb655663
Jeeho Park <devpark29@gmail.com>
2018-10-04 13:04:00 +0900

    IndexView로 /polls/cbv/ 구현

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 # app_name = 'polls'
@@ -13,12 +13,20 @@ from . import views
 # ]
 # from django.urls import path
 #
-from .views import fbv as views
-
+from .views import cbv, fbv as views
 app_name = 'polls'
+
+urlpatterns_cbv = [
+    path('', cbv.IndexView.as_view(), name='index'),
+]
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.result, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # /polls/cbv/로 시작하는 URL요청은
+    # 위의 urlpatterns_cbv리스트 내의 내요엥서 처리
+    path('cbv/', include(urlpatterns_cbv))
 ]
